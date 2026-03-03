8ba07d85f554b5f73fd41637573c78e559444eee
salesduo-arnav <arnav@salesduo.com>
2026-03-03 16:39:14 +0530

    fix: Logging

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -23,7 +23,9 @@ export const logger = winston.createLogger({
         isProd ? json() : combine(colorize(), myFormat)
     ),
     transports: [
-        new winston.transports.Console(),
+        new winston.transports.Console({
+            silent: process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'testing',
+        })
     ],
 });
 
